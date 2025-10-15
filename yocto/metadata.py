5f3e7b8bd1d4dc2618dca47671272a1951c52c37
cdrappi <c@seismic.systems>
2025-10-15 12:28:43 -0400

    use ruff

---
+++ yocto/metadata.py
@@ -0,0 +1,53 @@
+import json
+from typing import Dict, Tuple
+from pathlib import Path
+
+from yocto.paths import BuildPaths
+from yocto.measurements import Measurements
+
+
+def load_metadata(home: str) -> Dict[str, Dict]:
+    with open(BuildPaths(home).deploy_metadata) as f:
+        return json.load(f)
+
+
+def write_metadata(metadata: Dict[str, Dict], home: str):
+    with open(BuildPaths(home).deploy_metadata, "w+") as f:
+        json.dump(metadata, f, indent=2)
+
+
+def remove_vm_from_metadata(name: str, home: str):
+    metadata = load_metadata(home)
+    resources = metadata.get("resources", {})
+    if name not in resources:
+        return
+    resources.pop(name)
+    metadata["resources"] = resources
+    write_metadata(metadata, home)
+
+
+def remove_artifact_from_metadata(name: str, home: str):
+    metadata = load_metadata(home)
+    artifacts = metadata.get("artifacts", {})
+    if name not in artifacts:
+        return
+    artifacts.pop(name)
+    metadata["artifacts"] = artifacts
+    write_metadata(metadata, home)
+
+
+def load_artifact_measurements(artifact: str, home: str) -> Tuple[Path, Measurements]:
+    artifacts = load_metadata(home).get("artifacts", {})
+    if artifact not in artifacts:
+        raise ValueError(
+            f"Could not find artifact {artifact} "
+            f"in {BuildPaths(home).deploy_metadata}"
+        )
+    image_path = BuildPaths(home).artifacts / artifact
+    artifact = artifacts[artifact]
+    if not image_path.exists():
+        raise FileNotFoundError(
+            f"Artifact {artifact} is defined in the deploy metadata, "
+            "but the corresponding file was not found on the machine"
+        )
+    return image_path, artifact["image"]
